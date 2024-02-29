7e65c58e384993f42c03a3fff310581421a56ef2
João <joaombmrodrigues@gmail.com>
2024-02-29 21:58:10 +0000

    Cria programa que contém todos os tipos de variáveis que conheço

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //all variable types
+            //sbyte
+            sbyte sbytemax = sbyte.MaxValue;
+            sbyte sbytemin = sbyte.MinValue;
+            //short
+            short shortmax = short.MaxValue;
+            short shortmin = short.MinValue;
+            //int
+            int intmax = int.MaxValue;
+            int intmin = int.MinValue;
+            //long
+            long longmax = long.MaxValue;
+            long longmin = long.MinValue;
+            //byte
+            byte bytemax = byte.MaxValue;
+            byte bytemin = byte.MinValue;
+            //ushort
+            ushort ushortmax = ushort.MaxValue;
+            ushort ushortmin = ushort.MinValue;
+            //uint
+            uint uintmax = uint.MaxValue;
+            uint uintmin = uint.MinValue;
+            //ulong
+            ulong ulongmax = ulong.MaxValue;
+            ulong ulongmin = ulong.MinValue;
+            //char
+            char charmax = char.MaxValue;
+            char charmin = char.MinValue;
+            //float
+            float floatmax = float.MaxValue;
+            float floatmin = float.MinValue;
+            //double
+            double doublemax = double.MaxValue;
+            double doublemin = double.MinValue;
+            //decimal
+            decimal decimalmax = decimal.MaxValue;
+            decimal decimalmin = decimal.MinValue;
+        }
+    }
+}
