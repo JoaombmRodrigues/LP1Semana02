bf10aa7700639d86c57614957d8a52409f4a7b45
João <joaombmrodrigues@gmail.com>
2024-02-29 20:57:41 +0000

    Cria um programa que pede altura de um cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for height
+            Console.Write("Insira uma altura para um cilindro: ");
+            string altura = Console.ReadLine();
+            
+        }
+    }
+}
