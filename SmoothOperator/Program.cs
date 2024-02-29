1cfd1c157a49463167ae03f5981e38410c3e9640
João <joaombmrodrigues@gmail.com>
2024-02-29 19:54:23 +0000

    Cria um programa que pede ao utilizador um número inteiro não negativo

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for input
+            Console.Write("Insere um número inteiro não negativo: ");
+            string response = Console.ReadLine();
+        }
+    }
+}
