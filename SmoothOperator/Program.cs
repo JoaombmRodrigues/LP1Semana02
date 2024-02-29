d54db56391f8a8094eaff89ef59779b961f3d0b8
João <joaombmrodrigues@gmail.com>
2024-02-29 19:58:33 +0000

    Converte o input do usuário para uma variável n do tipo byte

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -9,6 +9,8 @@ namespace SmoothOperator
             //ask user for input
             Console.Write("Insere um número inteiro não negativo: ");
             string response = Console.ReadLine();
+            //convert string to byte
+            byte n = byte.Parse(response);
         }
     }
 }
